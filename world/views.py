bfffc731d2f4c541926712751c54903b6efb50c5
Dan Leehr <dan.leehr@nescent.org>
2013-09-27 16:02:20 -0400

    Added search for nearby countries

--- world/views.py
+++ world/views.py
@@ -1,5 +1,7 @@
 # Create your views here.
 from django.contrib.gis.geos.point import Point
+from django.contrib.gis.measure import Distance
+from django.core.exceptions import ObjectDoesNotExist
 
 from django.shortcuts import render
 from world.forms import SearchForm
@@ -13,8 +15,13 @@ def search_form(request):
 		if form.is_valid():
 			cd = form.cleaned_data
 			# Now find the objects
-			feature = WorldBorder.objects.get(mpoly__intersects=cd['point'])
-			response_dict = {'feature': feature}
+			try:
+				feature  = WorldBorder.objects.get(mpoly__intersects=cd['point'])
+			except ObjectDoesNotExist:
+				feature = None
+			# Also find things say, within 100 miles?
+			nearby= WorldBorder.objects.filter(mpoly__distance_lte=(cd['point'], Distance(mi=100)))
+			response_dict = {'feature': feature, 'nearby': nearby}
 			return render(request, 'search_results.html', response_dict)
 
 	else:
